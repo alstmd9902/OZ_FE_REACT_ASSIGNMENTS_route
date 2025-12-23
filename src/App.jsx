e18989da44e6df4fdffa77c48a0fd67c7f92134d
Seungyeon Oh <alstmd9902@naver.com>
2025-12-24 04:39:14 +0900

    feat: React Router로 페이지 라우팅 구조 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,49 @@
+import { BrowserRouter, Link, Route, Routes, Outlet } from "react-router-dom";
 import "./App.css";
 import headerStyle from "./assets/styles/header.module.css";
-/* Layout 컴포넌트
-1. /home으로 라우팅할 수 있는 <Link/>를 선언하세요.
-2. /about으로 라우팅할 수 있는 <Link/>를 선언하세요.
-3. /contact으로 라우팅할 수 있는 <Link/>를 선언하세요.
-4. 정의되지 않은 주소로 라우팅할 수 있는 <Link/>를 선언하세요.
-  4-1. 무작위 경로를 할당하거나, Math.random()을 활용할 수 있습니다.
-5. Layout 컴포넌트는 "모든" 컴포넌트에서 보여야 합니다. <Outlet/>를 추가하세요.
-*/
+
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import ErrorPage from "./pages/ErrorPage";
+
+/* Layout 컴포넌트 */
 function Layout() {
+  // const randomPath = `/random-${Math.floor(Math.random() * 1000)}`;
+
   return (
     <>
       <header className={headerStyle.header}>
         <div>Router Practice</div>
         <div className={headerStyle.headerLinks}>
-          {/* 여기에 Link 컴포넌트를 할당하세요. */}
+          <Link to="/home">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          {/* <Link to={randomPath}>Random</Link> */}
         </div>
       </header>
-      {/* 이곳에 Outlet을 할당하세요. */}
+
+      {/* 모든 페이지가 이 아래에 렌더링됨 */}
+      <Outlet />
     </>
   );
 }
 
-/* App 컴포넌트
-1. 모든 라우팅을 감싸는 BrowserRouter를 선언하세요.
-2. Routes를 선언하세요.
-3. 모든 라우팅에서 보이는 Layout의 Route를 선언하세요.
-4. / 라우팅에서 보이는 <HomePage/> 컴포넌트를 라우팅에 연결하세요.
-5. /home 라우팅에서 보이는 <HomePage/> 컴포넌트를 라우팅에 연결하세요.
-6. /about 라우팅에서 보이는 <AboutPage/> 컴포넌트를 라우팅에 연결하세요.
-7. /contact 라우팅에서 보이는 <ContactPage/> 컴포넌트를 라우팅에 연결하세요.
-8. /* 모든 잘못된 라우팅에서 보이는 <ErrorPage/> 컴포넌트를 라우팅에 연결하세요.
-*/
+/* App 컴포넌트 */
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
